5ba00d855df2e53528d5cefa514dd0adf4b809d7
timothycarambat <rambat1010@gmail.com>
2025-08-19 22:57:31 -0700

    uncheck tool debug

--- src/utils/ToolsManager/tools/webScraping/index.ts
+++ src/utils/ToolsManager/tools/webScraping/index.ts
@@ -1,6 +1,6 @@
 import { IAgentWebSearchCitation } from "@/database/models/WorkspaceChat";
 import { IStreamEvent } from "@/utils/AiProviders/baseOpenAILikeProvider";
-import { getOrigin } from "@/utils/formatters";
+import { getOrigin, safeJsonParse } from "@/utils/formatters";
 import webscraper from "./webscraper";
 
 export default {
@@ -27,27 +27,27 @@ export default {
         },
     },
     config: {},
-    execute: async function (url: string, streamEmitter: (event: IStreamEvent, data: any) => void): Promise<string> {
+    execute: async function (args: { url: string } | string, streamEmitter: (event: IStreamEvent, data: any) => void): Promise<string> {
         try {
+            const url = typeof args === 'string' ? safeJsonParse(args)?.url : args?.url;
             if (!url) return `No URL provided. No results were found.`;
             const hostname = getOrigin(url);
 
             // Ensure the URL is valid and always starts with https
             // Since we are using a webview we should do this.
             let validUrl = url;
-            debugger
             const protocolRegex = /^https?:\/\//i;
             if (!protocolRegex.test(validUrl)) validUrl = `https://${validUrl}`;
             else if (validUrl.match(/^http:\/\//i)) validUrl = validUrl.replace(/^http:\/\//i, 'https://');
 
-            validUrl = new URL(validUrl).toString();
-            const scrapeResult = await webscraper.scrape(validUrl);
+            validUrl = new URL(validUrl);
+            const scrapeResult = await webscraper.scrape(validUrl.toString());
 
             const citation = {
                 type: 'web-search',
                 reference: {
                     title: scrapeResult.title ?? hostname,
-                    url: validUrl,
+                    url: validUrl.toString(),
                     content: scrapeResult.content,
                 },
             } as IAgentWebSearchCitation;
@@ -59,36 +59,4 @@ export default {
             return `There was an error scraping the website. No content was found.`;
         }
     },
-    // execute: async function (args: { url: string } | string, streamEmitter: (event: IStreamEvent, data: any) => void): Promise<string> {
-    //     try {
-    //         const url = typeof args === 'string' ? safeJsonParse(args)?.url : args?.url;
-    //         if (!url) return `No URL provided. No results were found.`;
-    //         const hostname = getOrigin(url);
-
-    //         // Ensure the URL is valid and always starts with https
-    //         // Since we are using a webview we should do this.
-    //         let validUrl = url;
-    //         const protocolRegex = /^https?:\/\//i;
-    //         if (!protocolRegex.test(validUrl)) validUrl = `https://${validUrl}`;
-    //         else if (validUrl.match(/^http:\/\//i)) validUrl = validUrl.replace(/^http:\/\//i, 'https://');
-
-    //         validUrl = new URL(validUrl);
-    //         const scrapeResult = await webscraper.scrape(validUrl.toString());
-
-    //         const citation = {
-    //             type: 'web-search',
-    //             reference: {
-    //                 title: scrapeResult.title ?? hostname,
-    //                 url: validUrl.toString(),
-    //                 content: scrapeResult.content,
-    //             },
-    //         } as IAgentWebSearchCitation;
-
-    //         streamEmitter('report_citations', [citation]);
-    //         return scrapeResult.content;
-    //     } catch (e) {
-    //         console.error(`Web Scraping Error: ${e instanceof Error ? e.message : 'Unknown error'}`);
-    //         return `There was an error scraping the website. No content was found.`;
-    //     }
-    // },
 } as const;

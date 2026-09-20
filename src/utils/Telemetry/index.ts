8d93ba393313fc530fafb4e166acceb79f37b1e8
Timothy Carambat <rambat1010@gmail.com>
2026-09-19 21:59:14 -0700

    remove telem

--- src/utils/Telemetry/index.ts
+++ src/utils/Telemetry/index.ts
@@ -45,8 +45,6 @@ class Telemetry {
             MEMORY_SAVED: 'memory_saved',
             /** A scheduled job was created (payload: tools count, notify) */
             SCHEDULED_JOB_CREATED: 'scheduled_job_created',
-            /** A scheduled job run finished (payload: status, trigger, tools count) */
-            SCHEDULED_JOB_RAN: 'scheduled_job_ran',
 
             /** The user use the QR code to connect to an AnythingLLM intance */
             EXTERNAL_CONNECTION_ESTABLISHED: 'external_connection_established',
